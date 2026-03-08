8cfdbcac6f28e8e51d4c6b4d6f14a554173cdfe3
HaveYouPlayed <vladimir.leutar@gmail.com>
2026-03-08 17:55:21 +0100

    Add email filtering feature

--- filter_users.py
+++ filter_users.py
@@ -15,14 +15,23 @@ def filter_by_age(users, age_filter):
     ]
 
 
+def filter_by_email(users, email_filter):
+    return [
+        user for user in users
+        if email_filter.lower() in user["email"].lower()
+    ]
+
+
 with open("users.json", "r") as file:
     users = json.load(file)
 
 name_filter = input("Enter name to search: ")
 age_filter = int(input("Enter age to search: "))
+email_filter = input("Enter email to search: ")
 
 filtered_by_name = filter_by_name(users, name_filter)
 filtered_by_age = filter_by_age(users, age_filter)
+filtered_by_email = filter_by_email(users, email_filter)
 
 print("Users filtered by name:")
 for user in filtered_by_name:
@@ -30,4 +39,8 @@ for user in filtered_by_name:
 
 print("Users filtered by age:")
 for user in filtered_by_age:
+    print(user)
+
+print("Users filtered by email:")
+for user in filtered_by_email:
     print(user)
